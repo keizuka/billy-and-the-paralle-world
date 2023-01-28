1fecfb8ac369e911cab122b03e4b3536ca92028a
keizuka <benjamin.ardilouze212@gmail.com>
2023-01-28 15:19:32 +0100

    V 0.3
    map + player + move

--- game.py
+++ game.py
@@ -18,21 +18,43 @@ class Game:
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
 
         # generer le joueur
-        self.player = Player()
+        player_position = tmx_data.get_object_by_name("spawn")
+        self.player = Player(player_position.x, player_position.y)
 
         # dessiner le group de calque
         self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=10)
         self.group.add(self.player)
 
+    def handle_input(self):
+
+        pressed = pygame.key.get_pressed()
+
+        if pressed[pygame.K_UP]:
+            self.player.move_up()
+        elif pressed[pygame.K_DOWN]:
+            self.player.move_down()
+        elif pressed[pygame.K_LEFT]:
+            self.player.move_left()
+        elif pressed[pygame.K_RIGHT]:
+            self.player.move_right()
+
     def run(self):
+
+        clock = pygame.time.Clock()
 
         running = True
         while running:
 
+            self.handle_input()
+            self.group.update()
+            self.group.center(self.player.rect)
             self.group.draw(self.screen)
             pygame.display.flip()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+
+            clock.tick(60)
+
         pygame.quit()
--- player.py
+++ player.py
@@ -2,12 +2,25 @@ import pygame
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x, y):
         super(Player, self).__init__()
         self.sprite_sheet = pygame.image.load('assets/Male/Male 01-1.png')
         self.image = self.get_image(32, 0)
         self.image.set_colorkey([0, 0, 0])
         self.rect = self.image.get_rect()
+        self.position = [x, y]  # X/0 = gauche/droite || Y/1 = haut/bas
+        self.speed = 2
+
+    def move_right(self): self.position[0] += self.speed
+
+    def move_left(self): self.position[0] -= self.speed
+
+    def move_up(self): self.position[1] -= self.speed
+
+    def move_down(self): self.position[1] += self.speed
+
+    def update(self):
+        self.rect.topleft = self.position
 
     def get_image(self, x, y):
         image = pygame.Surface([32, 32])
