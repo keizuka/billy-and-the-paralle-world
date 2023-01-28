da24049096b162d4278333682a4adbd2a1618d33
keizuka <benjamin.ardilouze212@gmail.com>
2023-01-28 18:21:29 +0100

    V 0.4
    all collider OK

---
+++ tl32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.9.2" name="tl32x32" tilewidth="16" tileheight="16" tilecount="1200" columns="60">
+ <image source="32x32 tileset/32x32 first tileset.png" trans="000000" width="960" height="320"/>
+</tileset>
